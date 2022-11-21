5d8cb4d821cf1d0c1800e5b379984e90359c8814
Tyson Nguyen <briton2004@gmail.com>
2022-11-21 23:56:43 +0700

    this is just the same: d x!
    ds

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
+  signInWithGoogleRedirect,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase";
 
 const SignIn = () => {
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    };
+    loadData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -14,25 +27,11 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
 
 export default SignIn;
-
-/* auth,
-  signInWithGoogleRedirect,
-import { getRedirectResult } from "firebase/auth";
-import { useEffect } from "react";
-<button onClick={signInWithGoogleRedirect}>
-Sign in with Google Redirect
-</button>
-useEffect(() => {
-  const loadData = async () => {
-    const response = await getRedirectResult(auth);
-    if (response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user);
-    }
-  };
-  loadData();
-}, []); */ // SIGN IN WITH REDIRECT
