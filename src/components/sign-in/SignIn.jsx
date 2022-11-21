f9d05c1b5f81d28cb89d98889569dcd2394f74e6
Tyson Nguyen <briton2004@gmail.com>
2022-11-21 23:46:30 +0700

    added redirect sign in

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,18 +1,35 @@
 import React from "react";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
+  signInWithGoogleRedirect,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase";
+
 const SignIn = () => {
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    };
+    loadData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
-
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
