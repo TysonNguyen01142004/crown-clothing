4e113aeb423db38e58c01df97e085e87169c99d1
Tyson Nguyen <briton2004@gmail.com>
2022-11-22 18:50:49 +0700

    added sign up

--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -1,21 +1,61 @@
 import React from "react";
+import "./signUp.scss";
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmedPassword: "",
+};
 
 function SignUp() {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmedPassword } = formFields;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value }); //[name]: value is to update the appropriate fields in the NAME to the VALUE
+  };
+
   return (
     <div>
       <h1>Sign up with email and password</h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input required type="text" />
+        <input
+          required
+          type="text"
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
 
         <label>Email</label>
-        <input required type="email" />
+        <input
+          required
+          type="email"
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input required type="password" />
+        <input
+          required
+          type="password"
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input required type="password" />
+        <input
+          required
+          type="password"
+          onChange={handleChange}
+          name="confirmedPassword"
+          value={confirmedPassword}
+        />
 
         <button type="submit">Sign Up</button>
       </form>
